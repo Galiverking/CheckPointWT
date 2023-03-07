69deb7414a5ae8a51b1b3c4c6f1a43faf8545978
Galiverking <119554919+Galiverking@users.noreply.github.com>
2023-03-07 14:38:57 +0700

    Update Check2.lua

--- Check2.lua
+++ Check2.lua
@@ -1,9 +1,9 @@
 if game.PlaceId == 2753915549 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/WETHUBv1/WetHubv1/main/loading.lua"))();
   elseif game.PlaceId == 4442272183 then
-    game.Players.LocalPlayer:Kick("The third sea script is coming soon.  - สคริปต์ทะเลที่สามจะมาในเร็วๆนี้")
-  elseif game.PlaceId == 7449423635 then
     game.Players.LocalPlayer:Kick("The second sea script is coming soon.  -  สคริปต์ทะเลที่สองจะมาในเร็วๆนี้")
+  elseif game.PlaceId == 7449423635 then
+    game.Players.LocalPlayer:Kick("The third sea script is coming soon.  - สคริปต์ทะเลที่สามจะมาในเร็วๆนี้")
   else
     game.Players.LocalPlayer:Kick("Not Support Game - ไม่สนับสนุนเกมนี้")
 end
